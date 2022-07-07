526d99080d6f10bc8c4c105e596ed3c7f38045dc
Joe Shultz josephcshultz@gmail.com <josephcshultz@gmail.com>
2022-07-07 10:45:28 -0600

    Filled component with basic structure

---
+++ src/QuestionsAndAnswers/QandA.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class QandA extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Questions and Answers</h3>
+      </div>
+    )
+  }
+}
+
+export default QandA;
