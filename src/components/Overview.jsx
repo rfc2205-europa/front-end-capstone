0815079d0371594e77e2f58788325539d59286af
dkroll713 <dkroll713@gmail.com>
2022-07-07 11:03:00 -0500

    created base component for pull request practice

---
+++ src/components/Overview.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Product Overview</h3>
+        <p>Hello product overview world</p>
+      </div>
+    )
+  }
+}
+
+export default Overview
