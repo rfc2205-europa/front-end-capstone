e9e3129bfc5cfbe56415ee087692e5d68929a356
dkroll713 <dkroll713@gmail.com>
2022-07-11 11:29:28 -0500

    saved axios request

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Gallery from './subcomponents/Gallery.jsx';
 
+const axios = require('axios');
+
 class Overview extends React.Component {
   constructor(props) {
     super(props);
   }
 
   render() {
+    axios({
+      url: 'http://localhost:3005/',
+      type: 'get',
+      data: {
+        api: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/products'
+      }
+    })
     return (
       <div>
         <h3>Product Overview</h3>
