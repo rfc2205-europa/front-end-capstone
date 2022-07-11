d5eb7cf4db0cabe3acdc8131ef3120792aad5a3f
Joe Shultz josephcshultz@gmail.com <josephcshultz@gmail.com>
2022-07-11 09:50:50 -0600

    before cors

--- src/components/QuestionsAndAnswers/QandA.jsx
+++ src/components/QuestionsAndAnswers/QandA.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 class QandA extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount(){
+    this.fetch();
+  }
+
+  fetch = () => {
+    axios.request({
+      method: 'get',
+      data: {
+        api: "https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/qa/questions?product_id=66642",
+      },
+      url: 'http://localhost:3005/qa/questions'
+    })
+    .then((data)=> {
+      console.log(data);
+    })
+    .catch((err)=> {
+      console.log('There was an error in fetching data: ', err);
+    })
   }
 
   render() {
--- src/components/QuestionsAndAnswers/QandA.test.js
+++ src/components/QuestionsAndAnswers/QandA.test.js
@@ -2,7 +2,7 @@ import renderer from 'react-test-renderer';
 import QandA from './QandA.jsx';
 import React from 'react';
 
-describe.only("QandA Component Rendering", function () {
+describe.only("QandA Component Testing", function () {
   it('renders the QandA component correctly', () => {
     const tree = renderer.create(<QandA/>);
     expect(tree).toMatchSnapshot();
