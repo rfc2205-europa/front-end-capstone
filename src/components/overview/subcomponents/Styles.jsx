cb90ddb238791008c43cf77bfd3638ebb239dd30
dkroll713 <dkroll713@gmail.com>
2022-07-13 11:45:06 -0500

    separate styles into rows of 4

--- src/components/overview/subcomponents/Styles.jsx
+++ src/components/overview/subcomponents/Styles.jsx
@@ -4,6 +4,35 @@ import StyleThumb from './StyleThumb.jsx'
 class Styles extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      arrayOfRows: []
+    }
+  }
+
+  createStyleRows = (styles) => {
+    var styles = this.props.styles.results;
+    var productId = this.props.styles.product_id;
+    var arrayOfRows = [];
+    var row = [];
+    for (var x = 0; x < styles.length; x++) {
+      var spliced = styles.splice(0, 1);
+      row.push(spliced[0]);
+      x--;
+      if (row.length === 4 || styles.length === 0) {
+        arrayOfRows.push(row)
+        row = [];
+      }
+    }
+    console.log('final array:', arrayOfRows)
+    this.setState({
+      arrayOfRows: arrayOfRows
+    })
+  }
+
+  componentDidMount(prevProps) {
+    if (this.props.styles.results) {
+      this.createStyleRows(this.props.styles.results);
+    }
   }
 
   render() {
