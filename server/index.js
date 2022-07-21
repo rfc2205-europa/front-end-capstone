190f8b2c2c8ded324271e992d7e5901f58918bd5
dkroll713 <dkroll713@gmail.com>
2022-07-21 10:05:03 -0500

    modifying server to serve static files

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use(express.static('public'));
+
 // add authentication header
 // send api request with originalUrl
 // then send api response back to client in response
